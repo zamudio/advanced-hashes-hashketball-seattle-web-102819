48e817b90a722b8e3edded481081d164cb139566
Michael Z <mziacm@gmail.com>
2019-10-15 07:27:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -221,15 +221,15 @@ def winning_team
   team_point_totals.max_by{|key,val| val}.first
 end
 
-def player_with_longest_name
+def player_with_most_of(statistic)
   player_name = nil
-  amount_of_stat = 0
+  i = 0
 
-  game_hash.each do |_team, game_data|
-    game_data[:players].each do |player|
+  game_hash.each do |team, stats|
+    stats[:players].each do |player|
       if player[statistic].is_a? String
-        if player[statistic].length > amount_of_stat
-          amount_of_stat = player[statistic].length
+        if player[statistic].length > i
+          i = player[statistic].length
           player_name = player[:player_name]
         end
       end
